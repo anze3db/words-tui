21b56582e442283567b1748a656b0d9fbd37b975
Anže Pečar <anze@pecar.me>
2023-08-04 08:21:23 +0100

    Fix name of .db file

--- src/words_tui/cli/__init__.py
+++ src/words_tui/cli/__init__.py
@@ -12,7 +12,7 @@ from words_tui.tui.db import init_db
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="words-tui")
-@click.option("--db", "-d", envvar="WORDS_TUI_DB", default=Path.home() / ".write-tui.db", help="Database file to use")
+@click.option("--db", "-d", envvar="WORDS_TUI_DB", default=Path.home() / ".words-tui.db", help="Database file to use")
 def words_tui(db: str):
     init_db(db)
     WordsTui().run()
