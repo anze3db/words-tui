9db7cbaf3f37a97bcfdf36b58eb6df9e85bcf47c
Anže Pečar <anze@pecar.me>
2023-08-11 08:00:48 +0100

    Add settings

--- src/words_tui/tui/db.py
+++ src/words_tui/tui/db.py
@@ -14,14 +14,23 @@ class Post(BaseModel):
     content = TextField()
     created_date = DateTimeField(default=datetime.datetime.now)
 
+class Settings(BaseModel):
+    key = TextField()
+    value = TextField()
+
 
 def get_posts() -> list[Post]:
     return list(Post.select().order_by(Post.created_date.desc()))
 
+def get_settings() -> Settings:
+    return Settings.get(Settings.key == "words_per_day")
 
 def init_db(db_path: str):
     db = SqliteDatabase(db_path)
     database_proxy.initialize(db)
 
     db.connect()
-    db.create_tables([Post])
+    db.create_tables([Post, Settings])
+    
+    # Initialize settings:
+    Settings.get_or_create(key="words_per_day", defaults={"value": "300"})
