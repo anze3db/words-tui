571e7e21d1183779184f28eaac942689341d9664
Anže Pečar <anze@pecar.me>
2023-08-11 08:27:24 +0100

    Add setting pane

--- src/words_tui/tui/app.py
+++ src/words_tui/tui/app.py
@@ -1,44 +1,88 @@
 import datetime
 
+from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Static
+from textual.containers import Vertical
+from textual.screen import ModalScreen
+from textual.validation import Number
+from textual.widgets import Footer, Input, Label, Static
 
-from words_tui.tui.db import Post, get_posts
+from words_tui.tui.db import Post, get_posts, get_settings
 from words_tui.tui.text_editor import TextEditor
 
 
-def get_post_icon(post: Post) -> str:
-    if len(post.content.split()) > 300:
+def get_post_icon(post: Post, words_per_day: str) -> str:
+    if len(post.content.split()) >= int(words_per_day):
         return "✅"
     elif post.created_date.date() == datetime.date.today():
         return "📝"
     return "❌"
 
 
-def get_post_summary(post: Post) -> str:
+def get_post_summary(post: Post, words_per_day: str) -> str:
     return " ".join(
         map(
             str,
             [
-                get_post_icon(post),
+                get_post_icon(post, words_per_day),
                 post.created_date.strftime("%Y-%m-%d"),
-                f"{len(post.content.split()):5}/300",
+                f"{len(post.content.split()):5}/{words_per_day}",
             ],
         )
     )
 
 
-def get_sidebar_text() -> str:
+def get_sidebar_text(words_per_day: str) -> str:
     posts = get_posts()
-    return "[bold] # Date      Words/Goal[/bold]\n" + "\n".join(map(get_post_summary, posts))
+    return "[bold] # Date      Words/Goal[/bold]\n" + "\n".join(get_post_summary(post, words_per_day) for post in posts)
+
+
+class SettingsScreen(ModalScreen):
+    CSS_PATH = "settings.css"
+    BINDINGS = [
+        ("escape", "dismiss", "Back"),
+        ("ctrl+c", "quit", "Quit"),
+    ]
+
+    def action_dismiss(self) -> None:
+        self.dismiss(get_settings())
+
+    def compose(self) -> ComposeResult:
+        words_per_day = get_settings()
+        with Vertical(id="grid"):
+            yield Label(
+                "Settings",
+                id="settings_label",
+            )
+            yield Label(
+                "Number of words per day",
+                id="per_day_label",
+            )
+            yield Input(
+                words_per_day.value,
+                id="per_day_input",
+                validators=[
+                    Number(minimum=1, maximum=9999),
+                ],
+            )
+        yield Footer()
+
+    @on(Input.Changed)
+    def show_invalid_reasons(self, event: Input.Changed) -> None:
+        # Updating the UI to show the reasons why validation failed
+        if not event.validation_result.is_valid:
+            return
+
+        words_per_day = get_settings()
+        words_per_day.value = event.input.value
+        words_per_day.save()
 
 
 class WordsTui(App):
     """A Textual app for writing."""
 
-    BINDINGS = [
-        ("ctrl+c", "quit", "Quit"),
-    ]
+    BINDINGS = [("ctrl+c", "quit", "Quit"), ("ctrl+s", "open_settings", "Settings")]
+    SCREENS = {"settings": SettingsScreen()}
 
     CSS_PATH = "app.css"
 
@@ -46,6 +90,7 @@ class WordsTui(App):
         super().__init__(*args, **kwargs)
         # TODO: Find a better place for this
         self.posts = get_posts()
+        self.words_per_day = get_settings()
 
         if not self.posts:
             # When we first initialize the app there are no posts in the database, so create the first one:
@@ -56,7 +101,9 @@ class WordsTui(App):
             start_date = latest_post.created_date.replace(hour=0, minute=0, second=0, microsecond=0)
 
             for day in range(missing_days):
-                self.posts.insert(0, Post.create(content="", created_date=start_date + datetime.timedelta(days=day + 1)))
+                self.posts.insert(
+                    0, Post.create(content="", created_date=start_date + datetime.timedelta(days=day + 1))
+                )
 
         current_post = [post for post in self.posts if post.created_date.date() == datetime.date.today()]
         self.current_post: Post = current_post[0]
@@ -76,12 +123,20 @@ class WordsTui(App):
         self.current_post.content = text
         self.current_post.save()
 
-        sidebar.update(get_sidebar_text())
+        sidebar.update(get_sidebar_text(self.words_per_day.value))
+
+    def action_open_settings(self) -> None:
+        def after_quit(words_per_day) -> None:
+            self.editor.focus()
+            self.words_per_day = words_per_day
+            self.update_word_count()
+
+        self.push_screen(SettingsScreen(), after_quit)
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
 
-        yield Static(get_sidebar_text(), id="sidebar")
+        yield Static(get_sidebar_text(self.words_per_day.value), id="sidebar")
         yield self.editor
         yield Footer()
 
--- src/words_tui/tui/db.py
+++ src/words_tui/tui/db.py
@@ -14,6 +14,7 @@ class Post(BaseModel):
     content = TextField()
     created_date = DateTimeField(default=datetime.datetime.now)
 
+
 class Settings(BaseModel):
     key = TextField()
     value = TextField()
@@ -22,8 +23,10 @@ class Settings(BaseModel):
 def get_posts() -> list[Post]:
     return list(Post.select().order_by(Post.created_date.desc()))
 
+
 def get_settings() -> Settings:
     return Settings.get(Settings.key == "words_per_day")
+
 
 def init_db(db_path: str):
     db = SqliteDatabase(db_path)
@@ -31,6 +34,6 @@ def init_db(db_path: str):
 
     db.connect()
     db.create_tables([Post, Settings])
-    
+
     # Initialize settings:
     Settings.get_or_create(key="words_per_day", defaults={"value": "300"})
